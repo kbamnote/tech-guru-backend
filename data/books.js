59718fe600aee4b4a2a09048abedf20845e1dfd7
kbamnote3 <kunalbamnote7@gmail.com>
2026-05-14 14:21:09 +0530

    twoo

--- data/books.js
+++ data/books.js
@@ -1,50 +1,50 @@
 const books = {
   kids_book_1: {
-    name: "Kids Ebook 1",
+    name: "Activity Book School Bus",
     driveLink: "https://drive.google.com/file/d/1YVIHGDqEADS6zY1TLJNYbpD5kRXNbBwJ/view?usp=sharing"
   },
   kids_book_2: {
-    name: "Kids Ebook 2",
+    name: "Coloring Books Collection",
     driveLink: "https://drive.google.com/file/d/1lh4EB389proUlIk5jmoKET9N9diXywse/view?usp=sharing"
   },
   kids_book_3: {
-    name: "Kids Ebook 3",
+    name: "Kids Cyber Security Guide",
     driveLink: "https://drive.google.com/file/d/1gR7MBc8UsYXVuCfZujKN16-drBIUHmDb/view?usp=sharing"
   },
   kids_book_4: {
-    name: "Kids Ebook 4",
+    name: "Fun with Letters and Numbers",
     driveLink: "https://drive.google.com/file/d/1dIICDVUgupfv4jvu938g4Ushg5K8NgU-/view?usp=sharing"
   },
   kids_book_5: {
-    name: "Kids Ebook 5",
+    name: "Fun Activity Books Combo",
     driveLink: "https://drive.google.com/file/d/1Rtxz_ETjN2Jr9GM2N2nxqb6WCQYHjlDS/view?usp=sharing"
   },
   kids_book_6: {
-    name: "Kids Ebook 6",
+    name: "Ramadan Kids Book",
     driveLink: "https://drive.google.com/file/d/1Kc_W44b6myw9cUneQxm8DuMzq0oZ-vK5/view?usp=sharing"
   },
   kids_book_7: {
-    name: "Kids Ebook 7",
+    name: "Kids Activity Busy Book",
     driveLink: "https://drive.google.com/file/d/1_WV0C6_Os4S97ReZPkp5Ud0x4c2vCbyU/view?usp=sharing"
   },
   kids_book_8: {
-    name: "Kids Ebook 8",
+    name: "Kids Funbook Adventures",
     driveLink: "https://drive.google.com/file/d/1OoNHQ3StvVJ5vCLkndqaE-H_0SGFUrP3/view?usp=sharing"
   },
   kids_book_9: {
-    name: "Kids Ebook 9",
+    name: "Kids Tracing and Coloring",
     driveLink: "https://drive.google.com/file/d/1UCYcLU_8eqM9FEDRP0QVxgQX7qvbl6j-/view?usp=sharing"
   },
   kids_book_10: {
-    name: "Kids Ebook 10",
+    name: "Nursery Activity Books",
     driveLink: "https://drive.google.com/file/d/1UC3r-dxWrVjS1qcKZmgnwaaYtydZ_Hvt/view?usp=sharing"
   },
   kids_book_11: {
-    name: "Kids Ebook 11",
+    name: "Ramadan Activity Booklet",
     driveLink: "https://drive.google.com/file/d/1o9YphoQO2sRC0NDu59d-fTCm9tq0T3AR/view?usp=sharing"
   },
   kids_book_12: {
-    name: "Kids Ebook 12",
+    name: "Summer Activity Book",
     driveLink: "https://drive.google.com/file/d/1d24Mf6j0TBReG2ZNjbDg60baQv-QyI3J/view?usp=sharing"
   }
 };
