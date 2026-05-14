9a94d0c412afaa8d3cb5b3509315720f67cbcb21
kbamnote <kunalbamnote7@gmail.com>
2026-05-14 22:03:26 +0530

    ch2343

--- data/books.js
+++ data/books.js
@@ -46,6 +46,44 @@ const books = {
   kids_book_12: {
     name: "Summer Activity Book",
     driveLink: "https://drive.google.com/file/d/1d24Mf6j0TBReG2ZNjbDg60baQv-QyI3J/view?usp=sharing"
+  },
+
+  // Self Help EBooks
+  self_help_all_parts: {
+    name: "Self Help EBook All Part",
+    driveLink: "https://drive.google.com/drive/folders/1E-RfuFX98cRF27ci3Pol5XiuOn757AcN?usp=sharing"
+  },
+  self_help_part_1: {
+    name: "Self Help EBook Part 1",
+    driveLink: "https://drive.google.com/drive/folders/1SKfLMPKFizYT5MN4TMX0vra3VMeAdV_0?usp=sharing"
+  },
+  self_help_part_2: {
+    name: "Self Help EBook Part 2",
+    driveLink: "https://drive.google.com/drive/folders/1nvP8LLgCSK73Em3d_YB1FrMqmxyp0cr1?usp=sharing"
+  },
+  self_help_part_3: {
+    name: "Self Help EBook Part 3",
+    driveLink: "https://drive.google.com/drive/folders/1cF-Ca0u16L-TFKdxjGP6rlOdhbrZqRNM?usp=sharing"
+  },
+  self_help_part_4: {
+    name: "Self Help EBook Part 4",
+    driveLink: "https://drive.google.com/drive/folders/1lN4uKmmZXdrS-6HlsuhcfABaJLXHiL6W?usp=sharing"
+  },
+  self_help_part_5: {
+    name: "Self Help EBook Part 5",
+    driveLink: "https://drive.google.com/drive/folders/1dzOCDx5Fo7zvNOShsnckGDSZtc4ubr5f?usp=sharing"
+  },
+  self_help_part_6: {
+    name: "Self Help EBook Part 6",
+    driveLink: "https://drive.google.com/drive/folders/1ZDpZVwo0afN3FByQdCuYp3KCSqURjkGx?usp=sharing"
+  },
+  self_help_part_7: {
+    name: "Self Help EBook Part 7",
+    driveLink: "https://drive.google.com/drive/folders/18AaNLD-m7U3WOhlcdm7WeBGEjqezQmpV?usp=sharing"
+  },
+  self_help_part_8: {
+    name: "Self Help EBook Part 8",
+    driveLink: "https://drive.google.com/drive/folders/1555l5bYmAozlfnhcPJCJ3LCQFpnilLYk?usp=sharing"
   }
 };
 
